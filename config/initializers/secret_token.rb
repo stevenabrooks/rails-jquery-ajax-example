04b7db149616e327570a42abc60b09e42154411b
Jonathan Grover <jonathangrover@Jonathans-MacBook-Air.local>
2013-08-05 17:29:55 -0400

    setup new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJqueryAjaxExample::Application.config.secret_token = '2473712eee09269fa51ff47ad5097403d4c3137ef93fe92f5169afe993b8a92f2da69d9b12fa61a55304b77688c2a0bb02284617cedff397cea23550e6389824'
